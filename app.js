802d53bde6fff5caaa1ce344c93074cfa17f1314
Camden Taylor <camdentaylor1992@gmail.com>
2019-02-02 12:30:28 -0700

    Added some more basic routes. Need to plan stucture of app before proceeding. Decide on music database API to use

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+const express  = require('express'),
+      mongoose = require('mongoose'),
+      app      = express();
+
+//App configuration.
+app.set('view engine', 'ejs')
 
 //Shit to add. 
 //Add routes to index (landing page)
+app.get('/', (req, res) => {
+    res.render('landing')
+});
 
-//Add routes to page to view your album reviews. 
+//Add routes to page to view your album reviews.
+app.get('/albums', (req, res) => {
+    res.render('albums/index');
+}); 
 
 //Add form to submit an album review
-
+app.get('/albums/new', (req, res) => {
+    res.render('albums/new')
+});
 //Edit an album review
 
 app.listen(3000, () => {
