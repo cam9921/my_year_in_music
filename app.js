e491911965cd5e8c0d434295a116df3f0089a75b
Camden Taylor <camdentaylor1992@gmail.com>
2019-02-02 11:11:02 -0700

    Added initial app.js and added dependencies

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+//Shit to add. 
+//Add routes to index (landing page)
+
+//Add routes to page to view your album reviews. 
+
+//Add form to submit an album review
+
+//Edit an album review
+
+app.listen(3000, () => {
+    console.log('Server is started on port 3000');
+});
